34621f0b641e17de2c0a950e37ae5240768f65ef
Greatonic <awosusigabriel2019@gmail.com>
2025-09-17 05:48:41 +0100

    Telegoat Commit

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,9 +2,9 @@ import type { TgCtx } from "../types";
 import { startMenu } from "../ui/keyboards";
 
 export async function handleStart(ctx: TgCtx) {
-    console.log("Bot is up and running")
+    // console.log("Bot is up and running")
   await ctx.setChatMenuButton({ type: "commands" }).catch(() => {});
-  await ctx.telegram.setMyDescription("The unofficial bot for Telegoat").catch(() => {});
+  await ctx.telegram.setMyDescription("The unofficial bot for TELEGOAT").catch(() => {});
   await ctx.reply(
     "Welcome to the unofficial Telegoat bot 🐐\nPick a mode:",
     startMenu()
